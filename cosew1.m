2c4c8f4187e8e964add4f168753af0bfe83a3ed4
Leonardo Martins <leonardo@venidera.com>
2013-10-11 11:01:21 -0300

    Added COSE-W1 weekly load profile

---
+++ cosew1.m
@@ -0,0 +1,77 @@
+% COSE-W1 Hydro-thermal Coordination Test System
+%
+% Copyright (c) 2013 Universidade Estadual de Campinas
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy 
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights 
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+% copies of the Software, and to permit persons to whom the Software is 
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
+
+% Weekly average load as a percentage of annual peak
+load_w = [
+    0.627074; %  Apr-27
+    0.651798; %  May-04
+    0.659290; %  May-11
+    0.641309; %  May-18
+    0.607595; %  May-25
+    0.674274; %  Jun-01
+    0.664534; %  Jun-08
+    0.671277; %  Jun-15
+    0.645055; %  Jun-22
+    0.565641; %  Jun-29
+    0.611341; %  Jul-06
+    0.600103; %  Jul-13
+    0.661386; %  Jul-20
+    0.542637; %  Jul-27
+    0.583222; %  Aug-03
+    0.601260; %  Aug-10
+    0.547898; %  Aug-17
+    0.545643; %  Aug-24
+    0.529860; %  Aug-31
+    0.586229; %  Sep-07
+    0.522345; %  Sep-14
+    0.544140; %  Sep-21
+    0.544140; %  Sep-28
+    0.558420; %  Oct-05
+    0.559172; %  Oct-12
+    0.601260; %  Oct-19
+    0.662138; %  Oct-26
+    0.664153; %  Nov-02
+    0.682164; %  Nov-09
+    0.705428; %  Nov-16
+    0.667905; %  Nov-23
+    0.706929; %  Nov-30
+    0.727942; %  Dec-07
+    0.750455; %  Dec-14
+    0.714433; %  Dec-21
+    0.646893; %  Dec-28
+    0.675410; %  Jan-04
+    0.658900; %  Jan-11
+    0.625880; %  Jan-18
+    0.660401; %  Jan-25
+    0.631133; %  Feb-01
+    0.624379; %  Feb-08
+    0.604867; %  Feb-15
+    0.556166; %  Feb-22
+    0.553911; %  Mar-01
+    0.537376; %  Mar-08
+    0.546395; %  Mar-15
+    0.529109; %  Mar-22
+    0.563681; %  Mar-29
+    0.541886; %  Apr-05
+    0.601260; %  Apr-12
+    0.566688  %  Apr-19
+];
